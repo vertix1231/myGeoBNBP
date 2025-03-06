8e7f3165ab13d5be36830a2f8a840dbca86f21ad
vertix1231 <irsyad.muhamad.andre@gmail.com>
2025-03-06 17:13:34 +0700

    first commit

--- myGeoBNBP/Scripts/Tsunami/Script1741240182066.groovy
+++ myGeoBNBP/Scripts/Tsunami/Script1741240182066.groovy
@@ -19,7 +19,7 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
 
-WebUI.navigateToUrl('https://mhews.geodashboard.io/en')
+WebUI.navigateToUrl(GlobalVariable.url)
 
 WebUI.maximizeWindow()
 
